d2deadb9cc49ae9b9bb55ab2d5bfda7978d39ca2
tisyani <tisoczki.janos@diak.szbi-pg.hu>
2025-10-15 07:43:33 +0200

    vegso verzio

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import random
 
 random_number = random.randint(1,6)
 
-answer = int(input("Kérlek adj meg egy számot: "))
+answer = int(input("Kérlek adj meg egy számot 1-6-ig: "))
 
-if answer > random_number :
+if answer > random_number and answer < 7:
     print("Nyertél!")
 elif answer < random_number :
     print("Vesztettél!")
 elif answer == random_number : 
     print("Döntetlen")
+elif answer not in [1, 2, 3, 4, 5, 6]:
+    print("Csak 1-től 6-ig lehet számot megadni")
 
 print (f"{random_number} volt a szám.")
